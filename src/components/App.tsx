f1f64d6dc895fc0f236cb05e08a46296029e8006
Ibraheemosule <ibraheemosule@gmail.com>
2023-12-13 14:10:27 +0100

    added fontawesome cdn to the project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <h1 className="text-3xl text-dark.yellow font-bold underline bg-gray-900">
       Hello world! This is the beginnng
+      <i className="fa fa-car" />
     </h1>
   );
 }
