876b3b55405375cb4a3a2a4a88e8e0c11f445266
Ibraheemosule <ibraheemosule@gmail.com>
2023-12-13 14:01:58 +0100

    added the base styles and colors for the project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline bg-gray-900">
+    <h1 className="text-3xl text-dark.yellow font-bold underline bg-gray-900">
       Hello world! This is the beginnng
     </h1>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        purple: '#7a6ba9',
+        'dark.purple': '#432c81',
+        'light.purple': '#ccc3ff',
+        'dark.brown': '#58315a',
+        orange: '#ff8989',
+        'dark.yellow': '#ecbb5f',
+        'dark.gray': '#868686',
+      },
+    },
+  },
   plugins: [],
 };
