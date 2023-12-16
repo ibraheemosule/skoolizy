5f58d78956e1860d8194646635bb8d6e87ac2be9
Ibraheemosule <ibraheemosule@gmail.com>
2023-12-16 23:47:32 +0100

    extracted the type to a seperate file

--- src/components/reusables/menu/Menu.tsx
+++ src/components/reusables/menu/Menu.tsx
@@ -1,22 +1,19 @@
-import { ReactNode, memo } from 'react';
+import { memo } from 'react';
+import { IChild } from 'src/ts-types/react-types';
 
-interface IMenu {
-  children: ReactNode | string;
-}
-
-const Menu = ({ children }: IMenu) => (
+const Menu = ({ children }: IChild) => (
   <nav className="border-t border-gray-300 mt-6 pt-4 font-bold">{children}</nav>
 );
 
-const Title = ({ children }: IMenu) => (
+const Title = ({ children }: IChild) => (
   <h2 className="_full  text-xl py-4">{children}</h2>
 );
 
-const Wrapper = ({ children }: IMenu) => (
+const Wrapper = ({ children }: IChild) => (
   <ul className=" text-gray-800">{children}</ul>
 );
 
-const Item = ({ children }: IMenu) => (
+const Item = ({ children }: IChild) => (
   <li className="group pl-6 py-4 text-gray-500 hover:text-black hover:bg-gray-100 cursor-pointer relative">
     <i className="fa fa-phone group-hover:text-dark.brown" />
     <span className="ml-3">{children}</span>
