90ae1fb0dd8663c147be35a429b2d8a568caa973
Ibraheemosule <ibraheemosule@gmail.com>
2023-12-23 18:22:53 +0100

    added carousel

--- src/components/pages/dashboard/hero/Hero.tsx
+++ src/components/pages/dashboard/hero/Hero.tsx
@@ -3,15 +3,43 @@ import { Heading1 } from 'components/reusables/ui/heading';
 import { BaseText } from 'components/reusables/ui/text';
 import { ActionBtn } from 'components/reusables/ui/buttons';
 import { Card } from 'components/reusables/ui/others';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import Carousel from 'components/reusables/carousel/Carousel';
 
 const Hero = () => (
-  <Card className="bg-purple.light mt-8 p-6 ">
-    <Heading1>Announcements</Heading1>
-    <BaseText className="mt-2 text-gray-500 font-light">
-      Upload a Prescription and Tell Us what you Need. We do the Rest!
-    </BaseText>
-    <ActionBtn className="mt-4 px-4 py-2">Read more</ActionBtn>
-  </Card>
+  <div className="mt-8">
+    <Carousel>
+      <>
+        <div className="item ">
+          <Card className="bg-purple.light p-6 ">
+            <Heading1>Announnts</Heading1>
+            <BaseText className="mt-2 text-gray-500 font-light">
+              Upload a Prescription and Tell Us what you Need. We do the Rest!
+            </BaseText>
+            <ActionBtn className="mt-4 px-4 py-2">Read more</ActionBtn>
+          </Card>
+        </div>
+        <div className="item ">
+          <Card className="bg-purple.light p-6 ">
+            <Heading1>Announcementdffads</Heading1>
+            <BaseText className="mt-2 text-gray-500 font-light">
+              Upload a Prescription and Tell Us what you Need. We do the Rest!
+            </BaseText>
+            <ActionBtn className="mt-4 px-4 py-2">Read more</ActionBtn>
+          </Card>
+        </div>
+        <div className="item ">
+          <Card className="bg-purple.light p-6 ">
+            <Heading1>Auncemedfjnts</Heading1>
+            <BaseText className="mt-2 text-gray-500 font-light">
+              Upload a Prescription and Tell Us what you Need. We do the Rest!
+            </BaseText>
+            <ActionBtn className="mt-4 px-4 py-2">Read more</ActionBtn>
+          </Card>
+        </div>
+      </>
+    </Carousel>
+  </div>
 );
 
 export default memo(Hero);
