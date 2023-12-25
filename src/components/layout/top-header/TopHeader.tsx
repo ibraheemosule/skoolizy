1a334d79d41e335ebf12b1ada39d2bd07aa30cee
Ibraheemosule <ibraheemosule@gmail.com>
2023-12-25 08:03:02 +0100

    fixed top header

--- src/components/layout/top-header/TopHeader.tsx
+++ src/components/layout/top-header/TopHeader.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, FC, SetStateAction, memo } from 'react';
 import SearchBar from 'components/reusables/search-bar/SearchBar';
+import Menu from 'src/assets/icons/Menu';
 
 interface ITopHeader {
   setToggleNav: Dispatch<SetStateAction<boolean>>;
@@ -10,15 +11,17 @@ const TopHeader: FC<ITopHeader> = ({ setToggleNav }) => (
     <button
       data-testid="menu-btn"
       onClick={() => setToggleNav(true)}
-      className="fa fa-bars shrink-0 md:hidden"
+      className="shrink-0 mr-4 md:hidden"
       type="button"
-    />
-    <div className="flex grow items-center justify-between">
-      <div className="w-2/3 md:w-1/2 lg:w-3/5 self-stretch">
+    >
+      <Menu />
+    </button>
+    <div className="flex grow gap-4 items-center justify-between">
+      <div className="grow md:w-1/2 lg:w-3/5 self-stretch">
         <SearchBar />
       </div>
       <div>
-        <div className="flex gap-3 rounded-lg border p-2 items-center border-gray-300">
+        <div className="flex gap-3 rounded-lg border p-1 sm:p-2 items-center border-gray-300">
           <img
             className="rounded-full object-fill w-[30px] h-[30px]"
             src="https://picsum.photos/200/300"
