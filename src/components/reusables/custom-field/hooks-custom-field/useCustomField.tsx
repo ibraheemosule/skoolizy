45486051852f52dcee9f95fb5856be2036ba2ad8
Ibraheemosule <ibraheemosule@gmail.com>
2024-04-21 07:47:27 +0100

    added the jsdoc for the cutomField hook function

--- src/components/reusables/custom-field/hooks-custom-field/useCustomField.tsx
+++ src/components/reusables/custom-field/hooks-custom-field/useCustomField.tsx
@@ -1,5 +1,21 @@
 import { useRef, useState } from 'react';
 
+/**
+ * @param {string | string []} initialValue  The initial value of the input field
+ * which can be a string or an array for multiple select option
+ * @param {string[]} dropdownList an array of strings for the dropdown commponents,
+ * this should be passed if the dropdownList needs to be filtered based on what
+ * is being typed in the input field of the customfield
+ * @returns {[value, setValue, filteredList, filterFn]} This custom hook returns
+ * value -  This is what is to be passed to the customField value prop
+ * setValue - This is what updates the value, it should be passed to the onChange
+ * or onSelect Prop of the customField
+ * filteredList -  this is the list that should be used to map the Dropdown component
+ * if you want the dropdown to be filterable
+ * filterFn -  this is the function that handles the filtering of the list. It should
+ * be passed to the filterFn prop of the customField
+ */
+
 const useCustomField = <T extends string | string[]>(
   initialValue: T,
   dropDownist: string[]
