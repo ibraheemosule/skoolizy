d5085c191e0b3845626626ccfe32882466dc5321
Akintobi Moyinoluwa Ezekiel <akintobi41moyinoluwa@gmail.com>
2024-05-08 12:19:47 +0100

    g

--- src/utils/format/index.ts
+++ src/utils/format/index.ts
@@ -8,14 +8,15 @@ export const capitalizeChar = (str: string) => {
 };
 
 export const formatDate = (str: string) => {
-  const newDate = new Date(str) || new Date();
+  const newDate =
+    new Date(str).toString() === 'Invalid Date' ? new Date() : new Date(str);
   const getDate = `${newDate.getDate()}/${
     newDate.getMonth() + 1
   }/${newDate.getFullYear()}`;
   const hour = newDate.getUTCHours();
   const minute = `:${newDate.getUTCMinutes()}`;
-  const getTime = `${hour > 12 ? hour - 12 : hour < 1 ? 12 : hour}${minute}${
-    hour > 11 ? 'pm' : 'am'
-  }`;
+  const getTime = `${hour > 12 ? hour - 12 : hour < 1 ? 12 : hour}${
+    +minute < 10 ? `0${minute}` : minute
+  }${hour > 11 ? 'pm' : 'am'}`;
   return { getDate, getTime };
 };
