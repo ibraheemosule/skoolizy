47445144e0e7375f0eeba308ed18c465c060ca8a
Ibraheemosule <ibraheemosule@gmail.com>
2024-05-14 23:08:11 +0100

    added date range

--- src/components/pages/Students/Detail/routes/Attendance/Options/ReportAbsence/index.tsx
+++ src/components/pages/Students/Detail/routes/Attendance/Options/ReportAbsence/index.tsx
@@ -1,10 +1,11 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import { BoldText } from '~reusables/ui/Text';
 import Modal from '~components/reusables/Modal';
 import CustomField from '~reusables/CustomField';
 import useCustomField from '~reusables/CustomField/hooks-custom-field/useCustomField';
 
 const ReportAbsence = ({ closeModal }: { closeModal: () => void }) => {
+  const [classs, setClasss] = useState('today');
   const [year, setYear] = useCustomField('');
 
   return (
@@ -14,27 +15,49 @@ const ReportAbsence = ({ closeModal }: { closeModal: () => void }) => {
       content={
         <div className="text-left mb-4">
           <div className="mt-4">
-            <BoldText>From:</BoldText>
+            <BoldText>Select Day:</BoldText>
             <div className="mt-1">
               <CustomField
-                type="date"
-                onChange={setYear}
-                field="input"
-                value={year}
-              />
+                onSelect={(v) => setClasss(v)}
+                field="select"
+                value={classs}
+              >
+                <CustomField.DropdownWrapper>
+                  {['today', 'more days'].map((opt) => (
+                    <CustomField.Dropdown key={opt} value={opt}>
+                      <span className="capitalize">{opt}</span>
+                    </CustomField.Dropdown>
+                  ))}
+                </CustomField.DropdownWrapper>
+              </CustomField>
             </div>
           </div>
-          <div className="mt-4">
-            <BoldText>To:</BoldText>
-            <div className="mt-1">
-              <CustomField
-                type="date"
-                onChange={setYear}
-                field="input"
-                value={year}
-              />
-            </div>
-          </div>
+          {classs !== 'today' && (
+            <>
+              <div className="mt-4">
+                <BoldText>From:</BoldText>
+                <div className="mt-1">
+                  <CustomField
+                    type="date"
+                    onChange={setYear}
+                    field="input"
+                    value={year}
+                  />
+                </div>
+              </div>
+              <div className="mt-4">
+                <BoldText>To:</BoldText>
+                <div className="mt-1">
+                  <CustomField
+                    type="date"
+                    onChange={setYear}
+                    field="input"
+                    value={year}
+                  />
+                </div>
+              </div>
+            </>
+          )}
         </div>
       }
       action={() => null}
