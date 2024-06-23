392bc9b173a6772d9b0b9ec1de094aa88dc56d15
Ibraheemosule <ibraheemosule@gmail.com>
2024-06-23 06:59:23 +0100

    modified the table and set the student nav

--- src/components/pages/Students/index.tsx
+++ src/components/pages/Students/index.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom';
-import AsideAdmin from '~components/Layout/AsideAdmin';
+// import AsideAdmin from '~components/Layout/AsideAdmin';
 
 const Students = () => (
-  <section className="flex _full flex-wrap max-h-full lg:flex-nowrap gap-6 overflow-auto">
-    <div className="w-full min-h-full lg:w-3/5 xl:w-8/12  shrink-0">
+  <section className="flex _full flex-wrap max-h-full overflow-auto">
+    <div className="w-full">
       <Outlet />
     </div>
-    <div className="w-full hidden xlg:block lg:w-auto grow overflow-auto">
+    {/* <div className="w-full hidden xlg:block lg:w-auto grow overflow-auto">
       <AsideAdmin />
-    </div>
+    </div> */}
   </section>
 );
 
