4ff531ccd6ba6d13168b1b6b22da5e12258b3bd8
Ibraheemosule <ibraheemosule@gmail.com>
2024-07-05 16:42:06 +0100

    added padstart to the hour time in dateFormat function

--- src/utils/format/index.ts
+++ src/utils/format/index.ts
@@ -24,9 +24,9 @@ export const formatDate = (str: string = '', format = 12) => {
   )}:00`;
 
   if (format === 12) {
-    getTime = `${+hour > 12 ? +hour - 12 : +hour < 1 ? 12 : hour}:${minute}${
-      +hour > 11 ? 'pm' : 'am'
-    }`;
+    getTime = `${
+      +hour > 12 ? String(+hour - 12).padStart(2, '0') : +hour < 1 ? 12 : hour
+    }:${minute}${+hour > 11 ? 'pm' : 'am'}`;
   }
   return { getDate, getTime };
 };
