8026b12c52419dae62899c785886e9f4c980087d
Akintobi Moyinoluwa Ezekiel <akintobi41moyinoluwa@gmail.com>
2024-12-06 21:56:57 +0100

    Merge branch 'staging' of https://github.com/ibraheemosule/skoolizy into za

---
+++ src/store/userStore/utils-userStore.tsx
@@ -0,0 +1,22 @@
+import { decrypt, encrypt } from '~utils';
+
+const secureStorage = {
+  ...(window && window.localStorage),
+  getItem: (name: string): string | null => {
+    const data = localStorage.getItem(name);
+    if (!data) return null;
+    try {
+      return decrypt<string>(data);
+    } catch {
+      return null;
+    }
+  },
+  setItem: (name: string, value: string): void => {
+    const encryptedData = encrypt(value);
+    localStorage.setItem(name, encryptedData);
+  },
+  removeItem: (name: string): void => {
+    localStorage.removeItem(name);
+  },
+};
+export default secureStorage;
